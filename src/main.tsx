d64af4499ffea069c8dc88d2cbd79606668b0a6f
Eugene Yakhnenko <eugene.yakhnenko@virtru.com>
2025-09-26 15:02:05 -0700

    brachnch

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Debug from './components/Debug'
-import { ThemeProvider } from './theme/Theme.context'
-import type { ThemeOptions } from './theme/Theme.types'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Debug from "./components/Debug";
+import { ThemeProvider } from "./theme/Theme.context";
+import type { ThemeOptions } from "./theme/Theme.types";
 
 const themeConfig: ThemeOptions = {
   button: {
-    radius: 'sm',
-    shadow: 'sm',
+    radius: "sm",
+    shadow: "sm",
   },
 };
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider config={themeConfig}>
       <Debug />
     </ThemeProvider>
+    <div>Second branch</div>
   </StrictMode>
-)
+);
